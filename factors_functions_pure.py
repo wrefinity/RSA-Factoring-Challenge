4d68b5be58105e4d82e1b4103630f26f3fa76b24
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-09-18 21:55:43 +0100

    a trail divider for the prime number

--- factors_functions_pure.py
+++ factors_functions_pure.py
@@ -7,51 +7,44 @@ from time import time as timestamp
 
 def unix_time(function):
     '''Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
-    You can calculate the amount of used CPU-time used by your
-    function/callable by summing `user` and `sys`. `real` is just like the wall
-    clock.
-    Note that `sys` and `user`'s resolutions are limited by the resolution of
-    the operating system's software clock (check `man 7 time` for more
-    details).
+    calculates the amount of used CPU-time
     '''
-    start_time, start_resources = timestamp(), resource_usage(RUSAGE_SELF)
+    start_time, start_res = timestamp(), resource_usage(RUSAGE_SELF)
     function()
-    end_resources, end_time = resource_usage(RUSAGE_SELF), timestamp()
+    end_res, end_time = resource_usage(RUSAGE_SELF), timestamp()
 
     return "\nreal: {}\nuser: {}\nsys: {}\n".format(
         end_time - start_time,
-        end_resources.ru_utime - start_resources.ru_utime,
-        end_resources.ru_stime - start_resources.ru_stime)
+        end_res.ru_utime - start_res.ru_utime,
+        end_res.ru_stime - start_res.ru_stime)
 
 
-def trial_division(n: int) -> int:
+def trial_division(num: int):
     """
-    Finds the smallest divisor, if any, of a given number `n`
+    Finds the smallest divisor
     Returns:
         smallest divisor if found
-        0 if n is prime
+        0 if num is prime
     """
-    # TODO: Create a C library with this function to speed it up
-    while n % 2 == 0:
+    while num % 2 == 0:
         return 2
 
-    f = 3
-    while f * f <= n:
-        if n % f == 0:
-            return f
+    fac = 3
+    while fac ** 2 <= num:
+        if num % fac == 0:
+            return fac
         else:
-            f += 2
-    # n is prime
+            fac += 2
     return 1
 
 
 def print_factors():
 
-    with open(sys.argv[1], 'r') as prime:
-        line = prime.readline()
-        while line != '':
-            n = int(line)
-            rep = trial_division(n)
-            print("{}={}*{}".format(n, n//rep, rep))
+    with open(sys.argv[1], 'r') as src:
+        prime = src.readline()
+        while prime != '':
+            num = int(prime)
+            res = trial_division(num)
+            print("{}={}*{}".format(num, num//res, res))
 
-            line = prime.readline()
+            prime = src.readline()
